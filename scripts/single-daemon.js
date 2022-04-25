cbb4a67f70fbe4928f7e6caec5821dc48e687127
Heechul Ryu <ryuhcii@gmail.com>
2022-04-24 18:44:15 -0700

    Add single-daemon script

---
+++ scripts/single-daemon.js
@@ -0,0 +1,28 @@
+// Menu: Single Daemon
+// Description: Run command in background as singleton daemon
+// Author: Heechul Ryu
+
+import "@johnlindquist/kit";
+
+// commands that I would like to run as a singleton daemons
+const commands = [
+    "base16-shell-auto-reload-on-tmux",
+]
+
+const preview = async (name) => md(`${name}`);
+
+const results = commands.map((name) => ({
+    name,
+    value: name,
+    preview: async () => preview(name)
+}))
+
+let cmd = await arg("Select command:", results)
+
+// currently the dependancy is at https://github.com/ryuheechul/dotfiles/tree/master/bin/local
+// maybe this repo should just merge to the dotfiles repo above at one point
+await $`single-daemon ${cmd}`
+
+// if the PATH ever gets a problem
+// check by `cat ~/.kenv/.env`
+// run `~/.kit/bin/kit sync-path` to let Script Kit to set the proper PATH
